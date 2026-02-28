8573a10ebbb8cbedf16367187e1faba1b51813a9
GeorgiosKallarogiannis <georgios1kallarogiannis@outlook.de>
2026-02-28 12:18:56 +0100

    Refactor code into mvc structure
    This was done to clearly seperate concerns. Before the refactor
    code and presentation was in a single file.

--- src/index.php
+++ src/index.php
@@ -1,11 +1,7 @@
 <?php
 
-$dsn = "mysql:host=db;dbname=product_db;port=3306;charset=utf8mb4;";
+require "controller.php";
 
-$pdo = new PDO($dsn, "product_db_user", "secret");
+$controller = new Controller;
 
-$stmt = $pdo->query("SELECT * FROM product");
-
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-print_r($products);
+$controller->index();
---
+++ src/model.php
@@ -0,0 +1,15 @@
+<?php
+
+class Model
+{
+    public function getData():array
+    {
+        $dsn = "mysql:host=db;dbname=product_db;port=3306;charset=utf8mb4;";
+
+        $pdo = new PDO($dsn, "product_db_user", "secret");
+
+        $stmt = $pdo->query("SELECT * FROM product");
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
